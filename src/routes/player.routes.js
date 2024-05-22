71547a4abcc50975d1f16d0d63462d57cf5c125e
KOSASIH <kosasihg88@gmail.com>
2024-05-22 14:05:00 +0700

    Create player.routes.js

---
+++ src/routes/player.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const playerController = require('../controllers/player.controller');
+
+const router = express.Router();
+
+router.post('/', playerController.createPlayerController);
+router.post('/verify', playerController.verifyPlayerController);
+
+module.exports = router;

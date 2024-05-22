6d0c072ad7cfdd5afe63fca8f662f88b912065b0
KOSASIH <kosasihg88@gmail.com>
2024-05-22 14:58:42 +0700

    Create machine-learning.js

---
+++ artificial-intelligence/machine-learning.js
@@ -0,0 +1,38 @@
+class MachineLearning {
+  constructor(npcConstructor, trainingData, trainingLabels, learningRate, numIterations) {
+    this.npcConstructor = npcConstructor;
+    this.trainingData = trainingData;
+    this.trainingLabels = trainingLabels;
+    this.learningRate = learningRate;
+    this.numIterations = numIterations;
+
+    this.npcs = this.initializeNPCs();
+  }
+
+  initializeNPCs() {
+    let npcs = [];
+    for (let i = 0; i < this.trainingData.length; i++) {
+      npcs.push(new this.npcConstructor(this.trainingData[i]));
+    }
+    return npcs;
+  }
+
+  train() {
+    for (let i = 0; i < this.numIterations; i++) {
+      for (let j = 0; j < this.trainingData.length; j++) {
+        let npc = this.npcs[j];
+        let error = this.trainingLabels[j] - npc.predict();
+        npc.updateWeights(error, this.trainingData[j], this.learningRate);
+      }
+    }
+  }
+
+  updateNPCs() {
+    // Update NPCs based on new training data or other factors
+  }
+
+  run() {
+    this.train();
+    setInterval(() => this.updateNPCs(), 1000);
+  }
+}

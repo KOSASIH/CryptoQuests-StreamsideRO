b33e5def12480891dc4a3fac0e910bc17691736c
KOSASIH <kosasihg88@gmail.com>
2024-05-22 14:02:31 +0700

    Create transaction.js

---
+++ src/models/transaction.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db.config');
+
+const Transaction = sequelize.define(
+  'Transaction',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    playerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    piHash: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    status: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'pending',
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Transaction;

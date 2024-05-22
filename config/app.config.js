50007b1952afa031b74a0767ce826d05debcc445
KOSASIH <kosasihg88@gmail.com>
2024-05-22 14:06:41 +0700

    Create app.config.js

---
+++ config/app.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  appName: 'CryptoQuests-StreamsideRO',
+  appVersion: '1.0.0',
+  appPort: 3000,
+  appHost: 'localhost',
+  appBaseUrl: `http://${process.env.APP_HOST}:${process.env.APP_PORT}`,
+  appStaticDir: path.join(__dirname, '..', 'public'),
+  appLogDir: path.join(__dirname, '..', 'logs'),
+};

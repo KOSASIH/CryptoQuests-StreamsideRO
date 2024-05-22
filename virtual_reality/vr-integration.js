142658eefe4583dcb9b6642269216cd7df939d04
KOSASIH <kosasihg88@gmail.com>
2024-05-22 16:58:41 +0700

    Create vr-integration.js

---
+++ virtual_reality/vr-integration.js
@@ -0,0 +1,19 @@
+const { Client } = require('aframe-html-component');
+
+const client = new Client();
+
+client.on('didFinishLaunching', () => {
+  console.log('A-Frame server started');
+
+  // Register a new component
+  client.registerComponent('my-component', {
+    schema: {
+      value: { type: 'string' }
+    },
+
+    init: function () {
+      // Define the component's behavior
+      this.el.setAttribute('text', { value: this.data.value });
+    }
+  });
+      });

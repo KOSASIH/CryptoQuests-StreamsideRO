1d8188e4d7e5eaeb0c78997bb8c48da3d611ef9c
KOSASIH <kosasihg88@gmail.com>
2024-05-22 14:36:23 +0700

    Create quest.js

---
+++ models/quest.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const questSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rewards: {
+    type: Array,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Quest', questSchema);
